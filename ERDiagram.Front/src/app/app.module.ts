fb3f950a0575112d391b1edfb48a165f779d6cef
TempooDev <a.bermudez.freelance@gmail.com>
2023-05-16 23:37:31 +0200

    Add Auth0 and some test components

--- ERDiagram.Front/src/app/app.module.ts
+++ ERDiagram.Front/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment as env } from 'src/environments/environment';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
+import { AuthenticationButtonComponent } from './components/authentication-button/authentication-button.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+
+const appRoutes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginButtonComponent,
+    LogoutButtonComponent,
+    AuthenticationButtonComponent,
+    UserProfileComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AuthModule.forRoot({
+      domain: 'dev-va85m1jw6xjmlb5z.eu.auth0.com',
+      clientId: '7UTu83vCfuoiyCFbt843mS55q3lH8BoJ',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
