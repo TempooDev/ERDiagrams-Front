17619c59c4d6804f6c2de6ac3d64f217616bb416
TempooDev <a.bermudez.rodriguez@uma.es>
2024-03-08 19:46:53 +0100

    add files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@
 import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 export const routes = [
     {
         path: '', component: HomeComponent,
-        //canActivate:[AuthGuard]
+        canActivate: [AuthGuard]
     }
 ];
--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,13 +1,32 @@
 import {
+  ChangeDetectionStrategy,
   Component,
+  inject,
 } from '@angular/core';
+import { Observable, combineLatest, startWith } from 'rxjs';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { BoardService } from 'src/app/services/board.service';
+import { RelationShip } from 'src/app/entities/relationship';
+import { EntityDB } from 'src/app/entities/entitydb';
 @Component({
-    selector: 'app-board',
-    templateUrl: './board.component.html',
-    styleUrls: ['./board.component.scss'],
-    standalone: true,
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+  imports: [CommonModule],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BoardComponent {
+  private _boardService = inject(BoardService)
+  nodes$ = this._boardService.nodeDataArray$
+  links$ = this._boardService.linkDataArray$
 
+  addNode() {
+    const node: EntityDB = {
+      key: 'name',
+      items: []
+    }
+    this._boardService.setNode(node)
+  }
 
 }
